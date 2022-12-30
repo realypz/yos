e6dff68c335c00a8150cd866f0184114f32f6d5f
YANG Peizheng <realypz@realypz-AWR7>
2022-12-30 15:35:06 +0100

    Keyboard interrupt [Part 3, incomplete]
    What's new:
    * The keyboard map is added and make the basic character
      display correctly on screen.
    
      The character produced by combined keys (e.g. Shift + <key>)
      is not supported yet.
    
    Bug fix:
    * The if condition in keycode valid range is fixed.
      The bugged code use hex 0x128 as upperbound, while the
      fixed code uses decimal 128.

--- kernel/devices/keyboard.c
+++ kernel/devices/keyboard.c
@@ -6,6 +6,53 @@
 #define KEYBOARD_DATA_PORT 0x60
 #define KEYBOARD_STATUS_PORT 0x64
 
+#define NUM_SCANCODES 128
+
+uint8_t kKeyboardMap[NUM_SCANCODES] = 
+{
+	/*
+	This function has credit to keyboard_map.h in
+	https://stackoverflow.com/questions/37618111/keyboard-irq-within-an-x86-kernel.
+	*/
+
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',     /* 9 */
+  	'9', '0', '-', '=', '\b',     /* Backspace */
+  	'\t',                 /* Tab */
+  	'q', 'w', 'e', 'r',   /* 19 */
+  	't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
+    0,                  /* 29   - Control */
+  	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',     /* 39 */
+ 	'\'', '`',   0,                /* Left shift */
+ 	'\\', 'z', 'x', 'c', 'v', 'b', 'n',                    /* 49 */
+  	'm', ',', '.', '/',   0,                              /* Right shift */
+  	'*',
+    0,  /* Alt */
+  	' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+  	'-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+  	'+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
+
 void init_keyboard()
 {
 	unsigned char curmask_master = inb(0x21);
@@ -15,23 +62,24 @@ void init_keyboard()
 // https://www.youtube.com/watch?v=QRhFo-CnpvQ&t=452s
 void keyboard_isr_handler()
 {
-	static char *video_memory = (char*)0xb8000;
+	static uint8_t* video_memory = (uint8_t*)0xb8000;
 	unsigned char status = inb(KEYBOARD_STATUS_PORT);
 
 	if (status & 0x1)
 	{
-		char keycode = (char)inb(KEYBOARD_DATA_PORT);
-		if ((keycode > 0) && (keycode <= 0x128))
+		uint8_t const keycode = inb(KEYBOARD_DATA_PORT);
+		if ((keycode > 0) && (keycode < NUM_SCANCODES))
 		{
-			// TODO: Figure what is keycode (or key scancode),
-			//       and how to mapping it to the desired characters
-			//       on display.
-			*video_memory = keycode;
+			// TODO: Figure out how to display the characters from
+			//       combined keys, e.g. shitf + 1 to display '!'.
+			*video_memory = kKeyboardMap[keycode];
 			video_memory = video_memory + 2;
 		}
 	}
 
-	// Write end of interrupt (EOI)
+	// HINT: Write end of interrupt (EOI).
+	// These two lines are important without which the interrupt handler
+	// cannot be fired multiple times.
 	outb(PIC1_COMMAND_PORT, PIC2_EOI_CODE);
 	outb(PIC2_COMMAND_PORT, PIC2_EOI_CODE);
 }
